2126a2d86d36f439d07379e5bb0b76c819b3290a
VolodymyrBovsunovskiy <102419980+VolodymyrBovsunovskiy@users.noreply.github.com>
2023-08-18 20:30:54 +0300

    1

---
+++ js/01-gallery.js
@@ -0,0 +1,49 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryItemsMarkup = galleryItems
+  .map((item) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`;
+  })
+  .join("");
+
+const galleryEl = document.querySelector("div.gallery");
+
+galleryEl.innerHTML = galleryItemsMarkup;
+galleryEl.addEventListener("click", selectItem);
+
+function selectItem(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}">`,
+    {
+      onShow: (instance) => {
+        document.addEventListener("keydown", onEscKeyPress);
+      },
+      onClose: (instance) => {
+        document.removeEventListener("keydown", onEscKeyPress);
+      },
+    }
+  );
+  instance.show();
+
+  function onEscKeyPress(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
